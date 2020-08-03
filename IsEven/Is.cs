bcecc5bd083690b62bb302a35f4ee4dc2453dd0d
Tom Austin <Tom.Austin@streets-heaver.com>
2020-08-03 11:13:12 +0100

    Add project files.

---
+++ IsEven/Is.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace IsEven
+{
+    public static class Is
+    {
+        public static bool Even(int value)
+        {
+            return value % 2 == 0;
+        }
+
+        public static bool Odd(int value)
+        {
+            return value % 2 != 0;
+        }
+
+    }
+}
